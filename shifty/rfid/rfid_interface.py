480f0d42d4158524642220a5a61a389b2d32a703
Maro1 <ronning2504@gmail.com>
2020-08-27 18:57:21 +0200

    Created apps, rfid skeleton

---
+++ shifty/rfid/rfid_interface.py
@@ -0,0 +1,14 @@
+"""
+RFID Interface
+"""
+
+class RFIDInterface:
+
+    def __init__(self):
+        # TODO: Setup RFID stuff 
+        pass
+    
+    def read(self) -> str:
+        # TODO: Read RFID and return number as string
+        pass
+    
