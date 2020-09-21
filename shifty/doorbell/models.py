4b9457a4287b506bf8f10fd7bff44647c87f9a73
Maro1 <ronning2504@gmail.com>
2020-09-21 13:45:37 +0200

    Using database to store if is ringing

--- shifty/doorbell/models.py
+++ shifty/doorbell/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Ringing(models.Model):
+    """
+    Class for Attendance model
+    """
+    is_ringing = models.BooleanField(unique=True)
--- shifty/doorbell/views.py
+++ shifty/doorbell/views.py
@@ -2,19 +2,30 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http.response import HttpResponse
 from django.http import JsonResponse
+from .models import Ringing
 
-class Doorbell:
 
-    def __init__(self):
-        self.is_ringing = False
+@csrf_exempt
+def doorbell(request):
+    if request.method == 'POST':
+        set_ringing(True)
+        return HttpResponse(200)
 
-    @csrf_exempt
-    def doorbell(self, request):
-        if request.method == 'POST':
-            self.is_ringing = True
-            return HttpResponse(200)
+    elif request.method == 'GET':
+        ringing = get_ringing()
+        set_ringing(False)
+        return JsonResponse({'is_ringing': ringing})
 
-        elif request.method == 'GET':
-            return JsonResponse({'is_ringing': self.is_ringing})
+def set_ringing(value: bool):
+    ringing = Ringing.objects.all()
+    if ringing.count() == 0:
+        ringing.create(is_ringing=value)
+    else:
+        current_ringing = ringing.first()
+        setattr(current_ringing, 'is_ringing', value)
+        current_ringing.save()
+
+def get_ringing():
+    return Ringing.objects.first().is_ringing
 
         
