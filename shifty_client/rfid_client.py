aab1b4d86dae1da15c9ae86a6c78a573f79f6126
Bendik Nyhavn <bny@shifthyperloop.com>
2020-09-21 23:24:50 +0200

    Set mpg321 to lowest verbosity in RFID script

--- shifty_client/rfid_client.py
+++ shifty_client/rfid_client.py
@@ -1,44 +1,24 @@
 import os
-from urllib.parse import urlparse
-
+import time
 import requests
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-
-rfid_paths = ['checkin', 'checkout', 'rfid_register']
-
 reader = SimpleMFRC522()
 
 endpoint = os.environ.get('RFID_ENDPOINT')
 
-browser = webdriver.Chrome('chromedriver.exe')
-browser.fullscreen_window()
-
-browser.get(os.environ.get('INDEX_URL'))
-
 while True:
-    current_url = browser.current_url
-    url_path = urlparse(current_url)[2]
-    url_path = url_path.replace('/', '')
-
-    if url_path not in rfid_paths:
-        continue
-
     try:
         rfid, text = reader.read()
+        print('RFID: ', rfid)
+        os.system('mpg321 -q Ding-dong.mp3 &')
+        time.sleep(5)
 
     finally:
          GPIO.cleanup()
-
-    response = requests.post(url=endpoint, data={'rfid': rfid, 'type': url_path}).json()
-    if response['success'] and url_path == 'checkin':
-        browser.get(os.environ.get('INDEX_URL') + '/checkin_success')
-    if response['success'] and url_path == 'checkout':
-        browser.get(os.environ.get('INDEX_URL') + '/checkout_success')
-    if response['success'] and url_path == 'register':
-        browser.get(os.environ.get('INDEX_URL') + '/register_success')
+    try:
+         response = requests.post(url=endpoint, data={'rfid': rfid, 'type': 'checkin'}).json()
+    except Exception as e:
+         print('User does not exist!')
