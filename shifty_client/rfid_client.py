df44fa74105499429f18ccfeb6f0c89f7dc51525
Bendik Nyhavn <bny@shifthyperloop.com>
2020-09-21 23:44:40 +0200

    Added timestamp to RFID scan terminal echo, reduced sleep from 5s to 3s

--- shifty_client/rfid_client.py
+++ shifty_client/rfid_client.py
@@ -1,5 +1,6 @@
 import os
 import time
+import datetime
 import requests
 
 import RPi.GPIO as GPIO
@@ -10,15 +11,18 @@ reader = SimpleMFRC522()
 endpoint = os.environ.get('RFID_ENDPOINT')
 
 while True:
+
     try:
         rfid, text = reader.read()
-        print('RFID: ', rfid)
+        print(datetime.datetime.now().replace(microsecond=0).isoformat(' ', 'seconds') + ' - Scanned RFID: ' + str(rfid))
         os.system('mpg321 -q Ding-dong.mp3 &')
-        time.sleep(5)
+        time.sleep(3)
 
     finally:
          GPIO.cleanup()
+
     try:
          response = requests.post(url=endpoint, data={'rfid': rfid, 'type': 'checkin'}).json()
+
     except Exception as e:
-         print('User does not exist!')
+         print(datetime.datetime.now().replace(microsecond=0).isoformat(' ', 'seconds') + ' - User does not exist!')
