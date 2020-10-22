2c3763b60666de0bbad3b6700355488eccb4ce1e
Maro1 <ronning2504@gmail.com>
2020-10-22 20:46:31 +0200

    Printing POST

--- shifty/testing/views.py
+++ shifty/testing/views.py
@@ -5,7 +5,7 @@ import os
 def testing(request):
 	if request.method == "POST":
 		os.system(f"wall 'hello bendik, this is a post: {request}'")
-		print(f"POST req: {request}")
+		print(f"POST req: {request.POST}")
 	elif request.method == "GET":
 		os.system(f"wall 'Yo bendik, this is a get: {request}'")
 		print(f"GET req: {request}")
