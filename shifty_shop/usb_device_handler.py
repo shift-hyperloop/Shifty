255901966b54e0e8f406b74094a337df161e8868
Bendik Nyhavn <bny@shifthyperloop.com>
2020-11-09 20:18:42 +0100

    changed usb handler to a loop, removed prints from dist monitor

--- shifty_shop/usb_device_handler.py
+++ shifty_shop/usb_device_handler.py
@@ -4,6 +4,7 @@ import queue
 import time
 import RPi.GPIO as GPIO
 
+
 def get_distance():
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
@@ -31,25 +32,24 @@ def get_distance():
  
     return distance
 
-def monitor_distance():
+
+def monitor_distance(q):
     while True:
-        distance = get_distance()               # Get initial distance 
-        startTime = time.perf_counter()         # Get start time for when object enters range
+
+        distance = get_distance()   # Get initial distance
+        t0 = time.perf_counter()    # Get start time for when object enters range
 
         while distance < 5:
-            currentTime = time.perf_counter()   # Get current time
-            distance = get_distance()           # Update distance
+            t = time.perf_counter()     # Get current time
+            distance = get_distance()   # Update distance
 
-            if currentTime-startTime > 3:         # If time held is greater than 3 seconds, delete all 
-                q_distance.put("all")
-                print("all")
+            if distance >= 5:
 
-            elif currentTime-startTime > 0.5:       # If time held is greater than 0.5 seconds, delete last
-                q_distance.put("last")
-                print("last")
+                if t-t0 > 3:                # If time held is greater than 3 seconds, delete all
+                    q.put("all")
 
-
-
+                elif t-t0 > 0.5:            # If time held is greater than 0.5 seconds, delete last
+                    q.put("last")
 
 
 # method for grabbing and monitoring an USB device, and transferring the intercepted number sequences
@@ -67,8 +67,8 @@ def monitor_device(device_id, q):
                 if scanned_chars[-2] == 'ENTER':                                 # Looks for 2 consecutive ENTER presses
                     scanned_string = "".join([x for x in scanned_chars[:-2:2]])  # Concatenate every 2 number into str
                     q.put(scanned_string)                    # Puts dev name and num seq. into queue
-                    print(str(device.name) + ' did stuff.\n')                    # TODO: delete after debugging
                     scanned_chars = []                                           # Resets variable
+
 
 #Defining global objects
 q_RFID = queue.SimpleQueue()        # Queue used for transferring the intercepted number sequences
@@ -92,54 +92,16 @@ GPIO.output(GPIO_TRIGGER, False)    # Setting Trigger to False for redundancy
 # creates and starts threads for the RFID scanner and the barcode scanner. Daemon means they won't keep python waiting
 RFID = threading.Thread(target=monitor_device, args=('/dev/input/event0', q_RFID), daemon=True).start()
 barcode = threading.Thread(target=monitor_device, args=('/dev/input/event1', q_barcode), daemon=True).start()
-distance_sensor = threading.Thread(target=monitor_distance, daemon=True).start()
+distance_sensor = threading.Thread(target=monitor_distance, args=(q_distance,), daemon=True).start()
 
 
 if __name__ == '__main__':                      # Only if this script is run directly
-    import time
-    tilt = 0
 
     while True:
-        inp = input('Press enter to fetch scan from queue, or type "exit" to quit\n')
-
-        if inp.lower() == "exit":
-            break
-
-        elif inp == '':
-            tilt = 0
-            if q_RFID.qsize():
-                print('RFID: ' + str(q_RFID.get()))
-            elif q_barcode.qsize():
-                print('Barcode ID: ' + str(q_barcode.get()))
-            elif q_RFID.empty() and q_barcode.empty():
-                print('Queues are empty.')
-
-        elif inp:
-            tilt += 1
-            if tilt == 1:
-                print("Invalid input. Try again: ")
-            elif tilt == 2:
-                print("What did I just tell you. Read the damn instructions..: ")
-            elif tilt == 3:
-                print("""You're as useless as the "ueue" in "queue". """)
-            elif tilt == 4:
-                print("I would tell you to eat shit, but that would be cannibalism. ")
-            elif tilt == 5:
-                print("""I will smash your face into a car windshield, take your mother out for a nice seafood dinner
-                and then never call her again.""")
-            elif tilt == 6:
-                print("Remember to look both ways before you go fuck yourself. ")
-            elif tilt == 7:
-                print("You talk so much shit, I don't know whether to offer you breath mint or toilet paper. ")
-            elif tilt == 8:
-                print("I'm gonna harvest your toes. ")
-            elif tilt == 9:
-                print("""Type something again! I dare you! I double dare you motherf***ker! Type something one more"
-                goddamn time!""")
-            elif tilt == 10:
-                print()
-                print("OK BYE :*")
-                import os
-                time.sleep(3)
-                os.system('shutdown /s /t 1')
-                break
+        if q_RFID.qsize():
+            print('RFID: ' + str(q_RFID.get()))
+        if q_barcode.qsize():
+            print('Barcode ID: ' + str(q_barcode.get()))
+        if q_distance.qsize():
+            print('Distance message: ' + str(q_distance.get()))
+        time.sleep(0.05)
