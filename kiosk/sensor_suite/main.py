1028b3691369c68e316a57d9f95b4dee5e2a32fd
mkoshifthyperloop <mko@shifthyperloop.com>
2020-11-14 02:03:56 +0100

    hvis gud finnes

--- kiosk/sensor_suite/main.py
+++ kiosk/sensor_suite/main.py
@@ -1,4 +1,5 @@
 from sensor_handler import *
+#import send_data
 from flask import Flask
 import threading
 import RPi.GPIO as GPIO
@@ -35,7 +36,37 @@ if __name__ == '__main__':                      # Only if this script is run dir
     distance_sensor_thread = threading.Thread(target=monitor_distance, args=(q_distance,), daemon=True).start() # TODO: check!
     #web_server_thread = threading.Thread(target=start_web_server, args=(), daemon=True).start()
 
-    app = Flask("send_data")
+
+    @app.route('/RFID')
+    def rfid_get():
+        if q_rfid.qsize():
+            message = q_rfid.get()
+            print(message)
+            return message
+        else:
+            return "nothing new!"
+
+
+    @app.route('/barcode')
+    def barcode_get():
+        if q_barcode.qsize():
+            message = q_barcode.get()
+            print(message)
+            return message
+        else:
+            return "nothing new!"
+
+
+    @app.route('/distance')
+    def distance_get():
+        if q_distance.qsize():
+            message = q_distance.get()
+            print(message)
+            return message
+        else:
+            return "nothing new!"
+
+    app = Flask(__name__)
 
     try:
         app.run(debug=False, host='0.0.0.0')
