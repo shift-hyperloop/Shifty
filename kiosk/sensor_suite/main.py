3e8892cf84fb5e28ecdb08fbf5825bd3e1dbc258
Bendik Nyhavn <bny@shifthyperloop.com>
2020-11-14 01:44:42 +0100

    asdf

--- kiosk/sensor_suite/main.py
+++ kiosk/sensor_suite/main.py
@@ -1,4 +1,5 @@
 from sensor_handler import *
+import send_data
 from flask import Flask
 import threading
 import RPi.GPIO as GPIO
@@ -8,36 +9,35 @@ import time
 
 if __name__ == '__main__':                      # Only if this script is run directly
 
-    q_rfid = queue.SimpleQueue()  # Queue used for transferring the intercepted number sequences
-    q_barcode = queue.SimpleQueue()  # Queue used for transferring the intercepted number sequences
-    q_distance = queue.SimpleQueue()  # Queue for distance sensor
+    def stuff():
+        q_rfid = queue.SimpleQueue()  # Queue used for transferring the intercepted number sequences
+        q_barcode = queue.SimpleQueue()  # Queue used for transferring the intercepted number sequences
+        q_distance = queue.SimpleQueue()  # Queue for distance sensor
 
-    #devices = find_USB_devices()
-    #RFID_device_path = devices['RFID_device_path']
-    #barcode_device_path = devices['barcode_device_path']
+        #devices = find_USB_devices()
+        #RFID_device_path = devices['RFID_device_path']
+        #barcode_device_path = devices['barcode_device_path']
 
-    RFID_device_path = r"/dev/input/event2"
-    barcode_device_path = r"/dev/input/event1"
-
-
-    # creates and starts threads for the RFID scanner and the barcode scanner. Daemon means they won't keep python waiting
-    if RFID_device_path:
-        rfid_scanner_thread = threading.Thread(target=monitor_device, args=(RFID_device_path, q_rfid), daemon=True).start()
-    else:
-        print('Warning! No RFID scanner device found!')
-    time.sleep(5)
-
-    if barcode_device_path:
-        barcode_scanner_thread = threading.Thread(target=monitor_device, args=(barcode_device_path, q_barcode), daemon=True).start()
-    else:
-        print('Warning! No barcode scanner device found!')
-    time.sleep(5)
-
-    distance_sensor_thread = threading.Thread(target=monitor_distance, args=(q_distance,), daemon=True).start() # TODO: check!
-    #web_server_thread = threading.Thread(target=start_web_server, args=(), daemon=True).start()
+        RFID_device_path = r"/dev/input/event2"
+        barcode_device_path = r"/dev/input/event1"
 
 
-    app = Flask(__name__)
+        # creates and starts threads for the RFID scanner and the barcode scanner. Daemon means they won't keep python waiting
+        if RFID_device_path:
+            rfid_scanner_thread = threading.Thread(target=monitor_device, args=(RFID_device_path, q_rfid), daemon=True).start()
+        else:
+            print('Warning! No RFID scanner device found!')
+        time.sleep(5)
+
+        if barcode_device_path:
+            barcode_scanner_thread = threading.Thread(target=monitor_device, args=(barcode_device_path, q_barcode), daemon=True).start()
+        else:
+            print('Warning! No barcode scanner device found!')
+        time.sleep(5)
+
+        distance_sensor_thread = threading.Thread(target=monitor_distance, args=(q_distance,), daemon=True).start() # TODO: check!
+        #web_server_thread = threading.Thread(target=start_web_server, args=(), daemon=True).start()
+
 
     @app.route('/RFID')
     def rfid_get():
@@ -67,6 +67,8 @@ if __name__ == '__main__':                      # Only if this script is run dir
             return message
         else:
             return "nothing new!"
+
+    app = Flask(__name__)
 
     try:
         app.run(debug=True, host='0.0.0.0')
--- kiosk/sensor_suite/send_data.py
+++ kiosk/sensor_suite/send_data.py
@@ -1,34 +1,26 @@
-from flask import Flask
+@app.route('/RFID')
+def rfid_get():
+	if q_rfid.qsize():
+		message = q_rfid.get()
+		print(message)
+		return message
+	else:
+		return "nothing new!"
 
-def setup_web_server():
+@app.route('/barcode')
+def barcode_get():
+	if q_barcode.qsize():
+		message = q_barcode.get()
+		print(message)
+		return message
+	else:
+		return "nothing new!"
 
-	app = Flask(__name__)
-
-	@app.route('/RFID')
-	def rfid_get():
-		if q_rfid.qsize():
-			message = q_rfid.get()
-			print(message)
-			return message
-		else:
-			return "nothing new!"
-
-	@app.route('/barcode')
-	def barcode_get():
-		if q_barcode.qsize():
-			message = q_barcode.get()
-			print(message)
-			return message
-		else:
-			return "nothing new!"
-
-	@app.route('/distance')
-	def distance_get():
-		if q_distance.qsize():
-			message = q_distance.get()
-			print(message)
-			return message
-		else:
-			return "nothing new!"
-
-	return app
+@app.route('/distance')
+def distance_get():
+	if q_distance.qsize():
+		message = q_distance.get()
+		print(message)
+		return message
+	else:
+		return "nothing new!"
