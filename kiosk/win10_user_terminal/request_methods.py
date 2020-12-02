4d8b11c90af04143cdfac31a12fa3181ac2a21aa
Bendik Nyhavn <bny@shifthyperloop.com>
2020-12-03 00:19:12 +0100

    Adapted frontend to backend rework

--- kiosk/win10_user_terminal/request_methods.py
+++ kiosk/win10_user_terminal/request_methods.py
@@ -2,7 +2,7 @@ import requests
 url = "http://shifthyperloop01.it.ntnu.no:8079/kiosk"
 
 
-def get_product(barcode):
+def get_product_data(barcode):
     """
     Get product from database by barcode
     returns: [id] if not exists, which is a placeholder name for the product in the database which has just been created
@@ -11,7 +11,7 @@ def get_product(barcode):
     return requests.get(f"{url}?event=get_product&barcode={barcode}").text.split(",")
 
 
-def get_user(rfid):
+def get_user_data(rfid):
     """
     Get user from database by rfid
     returns: [id] if not exists, which is a placeholder name for the user in the database which has just been created
@@ -27,9 +27,16 @@ def post_purchase_order(rfid, barcode_list, total_price):
         total_price: total price of shopping cart
     """
     data = {
-        "event": "finish_purchase", "rfid": rfid,
-        "barcode": barcode_list, "total_price": total_price
+        "event": "finish_purchase",
+        "rfid": rfid,
+        "barcode": barcode_list,
+        "total_price": total_price
     }
-    response = requests.post(url, data=data)
-    if response.status_code != 200:
-        print("Fuck, something went wrong")
+
+    response_code = requests.post(url, data=data).status_code
+    print(response_code)
+
+    if response_code == 200:
+        return True
+    else:
+        return False
