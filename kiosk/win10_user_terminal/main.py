876f21db5e333f2e0f76e90616609b762a39c239
eiriksru <eirik.runshaug@shifthyperloop.com>
2020-12-03 18:30:20 +0100

    Successfull transaction thing

--- kiosk/win10_user_terminal/main.py
+++ kiosk/win10_user_terminal/main.py
@@ -166,6 +166,9 @@ def query_rfid_scanner(engine, q_cart):
                     mainWindow = engine.rootObjects()[0]
                     mainWindow.findChild(QtCore.QObject, "productString").clear()
                     mainWindow.findChild(QtCore.QObject, "priceString").clear()
+                    mainWindow.findChild(QtCore.QObject, "totalpricestring").clear().insert(0, "Balance" + balance)
+                    mainWindow.findChild(QtCore.QObject, "userstring").clear().insert(0, "Purchase successfull! Account charged "+tot_purchase_sum+",-")
+                    QtCore.QTimer.singleShot(1500, partial(enter_idle_screen, engine))
 
 
 def main_loop(engine, q_cart):
