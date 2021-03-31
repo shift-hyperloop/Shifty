2e1372ba1baafa8f4d87ab0f893d637661e487e3
Maro1 <ronning2504@gmail.com>
2021-03-31 15:12:30 +0200

    Office Status should now work once the app is installed

--- shifty/attendance/views.py
+++ shifty/attendance/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from slack import WebClient
+from slack.errors import SlackApiError
 
 from .models import Attendance, AtOffice
 from .models import RFIDUser
@@ -45,11 +46,7 @@ class RFIDView:
                     at_office_obj.save()
                     return JsonResponse(dict(success=True, type='check_in'))
 
-                elif attendance.check_out:
-                    Attendance.objects.create(user=user, check_in=current_time)
-                    at_office_num = at_office_obj.at_office
-                   # if at_office_num == 0:
-                    #    RFIDView.office_opened()
+                elif attendance.check_out:channel_id = "C01BV9EHN48"
                     at_office_num += 1
                    # RFIDView.update_at_office(at_office_num)
                     setattr(at_office_obj, 'at_office', at_office_num) 
@@ -73,25 +70,62 @@ class RFIDView:
                 return JsonResponse(dict(success=False))
 
     @staticmethod
-    def office_opened():
+    def _delete_messages():
         slack_api_token = os.environ.get('SLACK_API_TOKEN')
         client = WebClient(token=slack_api_token)
-        client.chat_update(channel='C01BV9EHN48', ts='1601053648.000100', text='Office is OPEN! :green_apple:')
+
+        conversation_history = []
+        channel_id = "C01BV9EHN48"
+
+        try:
+            # Get all messages and delete, should only be 2 messages for Office Status channel
+            
+            result = client.conversations_history(channel=channel_id)
+            conversation_history = result["messages"]
+
+            for message in conversation_history:
+                m_ts = message["ts"]
+                client.chat_delete(channel=channel_id, ts=m_ts)
+
+        except SlackApiError as e:
+            print("Error creating conversation: {}".format(e))
+
+    @staticmethod
+    def office_opened():
+        RFIDView._delete_messages()
+        slack_api_token = os.environ.get('SLACK_API_TOKEN')
+        client = WebClient(token=slack_api_token)
+        channel_id = "C01BV9EHN48"
+
+        try:
+            client.chat_postMessage(channel=channel_id, text='Office is OPEN! :green_apple:')
+        except SlackApiError as e:
+            print("Error posting message: {}".format(e))
+
 
     @staticmethod
     def office_closed():
+        RFIDView._delete_messages()
         slack_api_token = os.environ.get('SLACK_API_TOKEN')
         client = WebClient(token=slack_api_token)
-        client.chat_update(channel='C01BV9EHN48', ts='1601053648.000100', text='Office is CLOSED! :red_circle:')
+        channel_id = "C01BV9EHN48"
+
+        try:
+            client.chat_postMessage(channel=channel_id, text='Office is CLOSED! :red_circle:')
+        except SlackApiError as e:
+            print("Error posting message: {}".format(e))
+
     
     @staticmethod
     def update_at_office(at_office: int):
-        print("update")
         slack_api_token = os.environ.get('SLACK_API_TOKEN')
-        print("u1")
         client = WebClient(token=slack_api_token)
-        print("u2")
-        client.chat_update(channel='C01BV9EHN48', ts='1601054690.000600', text=f'Currently at office: {at_office}')
+        channel_id = "C01BV9EHN48"
+
+        try:
+            client.chat_postMessage(channel=channel_id, text=f'Currently at office: {at_office}')
+        except SlackApiError as e:
+            print("Error posting message: {}".format(e))
 
 
 
